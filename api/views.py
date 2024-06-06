f22697b1738306a494d5e6fd038bfce2a5bda0c0
Tony Sugent <tsugent@consumeraffairs.com>
2024-06-06 17:48:37 -0400

    add redirect view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic.base import View
 
-# Create your views here.
+from links.models import Link
+
+
+class LinkRedirect(View):
+    def get(self, *args, **kwargs):
+        uuid = kwargs["uuid"]
+        link = get_object_or_404(Link, path=uuid)
+        link.visits += 1
+        link.save()
+        return redirect(link.original_url)
